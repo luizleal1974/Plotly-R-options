8c8bd32d0d73b9c10a6f65783cbbc5deeda97a37
luizleal1974 <statistical.metrology@gmail.com>
2023-08-04 18:23:21 -0300

    Figura 1 (c)
    Gráfico de Barras

--- Figura_1_c.R
+++ Figura_1_c.R
@@ -71,4 +71,3 @@
             legend.key.size = unit(5, 'cm')
             )
   ggplotly(p) %>% layout(margin = list(b = 150, t = 150, l = 450, r = 450), hoverlabel = list(font = list(family = "Calibri", size = 30, color = 'white')))
-
