919b37798e8193d93c4127518750dc2863940a32
luizleal1974 <statistical.metrology@gmail.com>
2024-05-29 18:51:52 -0300

    Shiny app

--- R codes/app.R
+++ R codes/app.R
@@ -11,13 +11,13 @@
   library(plotly)
 
 # ui
-  ui <- fluidPage(    plotlyOutput(outputId = "PLOT_2_1"), dataTableOutput(outputId = "TAB_2_1")    )
+  ui <- fluidPage(    plotlyOutput(outputId = "PLOT_2_1"), DT::DTOutput(outputId = "TAB_2_1")    )
 
 # server
   server <- function(input, output) {
             values = c(0, 1, 2, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 9)
             output$PLOT_2_1 <- renderPlotly({   f(x = values, tab = FALSE)  }) # PLOT WILL BE SHOWN IN SHINY APP.
-            output$TAB_2_1 <- renderDataTable({ f(x = values, tab = TRUE)   }) # PLOT WILL NOT BE SHOWN IN SHINY APP.
+            output$TAB_2_1 <- DT::renderDT({ f(x = values, tab = TRUE)   }) # PLOT WILL NOT BE SHOWN IN SHINY APP.
             }
 
 # Run app
